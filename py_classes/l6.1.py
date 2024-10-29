ccecd7246f84931b95e2cd44d88e41b95d56495b
stefano.montanelli@unimi.it <stefano.montanelli@unimi.it>
2024-10-29 20:25:10 +0100

    added files of lecture on 20241028

--- py_classes/l6.1.py
+++ py_classes/l6.1.py
@@ -26,6 +26,38 @@ from termcolor import colored
 max_attempts = 6
 tiles = {"correct_place": "🟩", "correct_letter": "🟨", "incorrect": "⬛"}
 
+
+# check_guess returns colored letters and colored tiles that are the feedback on a letter-by-letter comparison of guess against target
+# this solution is not completely satisfactory. There are cases where the letters are not properly colored (can you find a solution?). Example:
+# guess: canal
+# target: medal
+def check_guess(guess, target):
+    # global means that a variable defined in the main code is visible and accessible in the function scope
+    global tiles
+
+    colored_letters = []
+    colored_tiles = []
+
+    for i, letter in enumerate(guess):
+        if guess[i] == target[i]:
+            # color in green
+            colored_letters.append(colored(letter, "green"))
+            colored_tiles.append(tiles["correct_place"])
+        elif guess[i] in target:
+            # color in yellow
+            colored_letters.append(colored(letter, "yellow"))
+            colored_tiles.append(tiles["correct_letter"])
+        else:
+            # color in grey
+            colored_letters.append(letter)
+            colored_tiles.append(tiles["incorrect"])
+
+        # print(colored_tiles)
+        # print("".join(colored_tiles))
+
+    return "".join(colored_letters), "".join(colored_tiles)
+
+
 # main
 # load the EN dictionary and isolate the 5-letter words
 with open("dataset/en_unigram_freq.csv", newline="") as f:
@@ -52,10 +84,56 @@ en_words_5 = [x.upper() for x in en_words if len(x) == 5]
 # the word frequencies could be used to make more probable the words that are less frequent
 # this way, it is possible to define the game difficulty
 target = choice(en_words_5)
+target = "MEDAL"
+print("the target is:" + target)
 
 print("Welcome to WORDLE!")
 print(f"Guess the target word, you have {max_attempts} tries.")
 
-# work on your solution, discussion in the next classes
+
+# list of previous attempts and colored guesses
+history = []
+colored_letters = []
+colored_tiles = []
+# current attempt
+attempt = 1
+
+# while loop: consider the number of attempts (that needs to be lower than the maximum)
+while attempt <= max_attempts:
+    # enter the guess
+    guess = input(f"Attempt number {attempt}. Insert a 5-letter word:")
+
+    guess = guess.upper()
+
+    if guess == target:
+        print("Good job! the target is guessed!")
+        # move attempts over the max, so the loop is stopped
+        attempt = max_attempts + 1
+
+    # check possible errors in the attempt:
+    # not in EN dict, not 5 letters, already in the history
+    if len(guess) != 5:
+        print("Please, insert a 5-letter word!")
+    elif not guess.isalpha():
+        print("Please, insert a 5-letter word!")
+    elif guess in history:
+        print("This guess is already tried, try another one!")
+    elif guess not in en_words_5:
+        print("Please, insert a word in the English dictionary")
+    else:
+        # if the guess is the target or provide a feedback
+        history.append(guess)
+        c_letters, c_tiles = check_guess(guess, target)
+
+        print(c_letters)
+        print(c_tiles)
+
+        colored_letters.append(c_letters)
+        colored_tiles.append(c_tiles)
+
+        # increase the counter
+        attempt += 1
+
+# provide a feedback when the target is missed
 
 sys.exit()
