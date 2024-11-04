9ef55000d9d85fd8fd07cd01f3853a63d3ae9754
stefano.montanelli@unimi.it <stefano.montanelli@unimi.it>
2024-11-04 22:34:56 +0100

    added files of lecture on 20241104

--- py_classes/l6.1.py
+++ py_classes/l6.1.py
@@ -21,9 +21,11 @@ import sys
 import csv
 from random import choice
 from termcolor import colored
+from datetime import datetime
 
 # setup
 max_attempts = 6
+save_on_file = True
 tiles = {"correct_place": "🟩", "correct_letter": "🟨", "incorrect": "⬛"}
 
 
@@ -31,7 +33,7 @@ tiles = {"correct_place": "🟩", "correct_letter": "🟨", "incorrect": "⬛"}
 # this solution is not completely satisfactory. There are cases where the letters are not properly colored (can you find a solution?). Example:
 # guess: canal
 # target: medal
-def check_guess(guess, target):
+def check_guess_old(guess, target):
     # global means that a variable defined in the main code is visible and accessible in the function scope
     global tiles
 
@@ -50,6 +52,41 @@ def check_guess(guess, target):
         else:
             # color in grey
             colored_letters.append(letter)
+            colored_tiles.append(tiles["incorrect"])
+
+        # print(colored_tiles)
+        # print("".join(colored_tiles))
+
+    return "".join(colored_letters), "".join(colored_tiles)
+
+
+def check_guess(guess, target):
+    # global means that a variable defined in the main code is visible and accessible in the function scope
+    global tiles
+
+    colored_letters = []
+    colored_tiles = []
+
+    # manage the green case (correct letters in proper position)
+    correct_letters = list(map(lambda x, y: x == y, guess, target))
+    # replace the correct letters with hyphen
+    target = "".join([y if not x else "-" for x, y in zip(correct_letters, target)])
+
+    # manage the yellow case (right letters in wrong position)
+    yellow_letters = list(map(lambda x: x in target, guess))
+
+    for i, (x, y) in enumerate(zip(correct_letters, yellow_letters)):
+        if x:
+            # color in green
+            colored_letters.append(colored(guess[i], "green"))
+            colored_tiles.append(tiles["correct_place"])
+        elif y:
+            # color in yellow
+            colored_letters.append(colored(guess[i], "yellow"))
+            colored_tiles.append(tiles["correct_letter"])
+        else:
+            # color in grey
+            colored_letters.append(guess[i])
             colored_tiles.append(tiles["incorrect"])
 
         # print(colored_tiles)
@@ -84,12 +121,13 @@ en_words_5 = [x.upper() for x in en_words if len(x) == 5]
 # the word frequencies could be used to make more probable the words that are less frequent
 # this way, it is possible to define the game difficulty
 target = choice(en_words_5)
-target = "MEDAL"
-print("the target is:" + target)
+# target = "MEDAL"
+# print the target for debugging
+print("the target is: " + target)
 
 print("Welcome to WORDLE!")
 print(f"Guess the target word, you have {max_attempts} tries.")
-
+print("Print 'quit' to finish the game.")
 
 # list of previous attempts and colored guesses
 history = []
@@ -97,43 +135,76 @@ colored_letters = []
 colored_tiles = []
 # current attempt
 attempt = 1
-
+is_guessed = False
+is_stopped = False
 # while loop: consider the number of attempts (that needs to be lower than the maximum)
-while attempt <= max_attempts:
-    # enter the guess
-    guess = input(f"Attempt number {attempt}. Insert a 5-letter word:")
+while (attempt <= max_attempts) and (not is_guessed) and (not is_stopped):
 
-    guess = guess.upper()
+    # loop over the input until a "rule-compliant" guess is inserted by the user
+    is_input_ok = False
+    while not is_input_ok:
+        # enter the guess
+        guess = input(f"Attempt number {attempt}. Insert a 5-letter word: ")
+        guess = guess.upper()
 
-    if guess == target:
-        print("Good job! the target is guessed!")
-        # move attempts over the max, so the loop is stopped
-        attempt = max_attempts + 1
+        if guess.lower() == "quit":
+            is_stopped = True
+            break
 
-    # check possible errors in the attempt:
-    # not in EN dict, not 5 letters, already in the history
-    if len(guess) != 5:
-        print("Please, insert a 5-letter word!")
-    elif not guess.isalpha():
-        print("Please, insert a 5-letter word!")
-    elif guess in history:
-        print("This guess is already tried, try another one!")
-    elif guess not in en_words_5:
-        print("Please, insert a word in the English dictionary")
-    else:
-        # if the guess is the target or provide a feedback
-        history.append(guess)
+        # check possible errors in the attempt:
+        # not in EN dict, not 5 letters, already in the history
+        if len(guess) != 5:
+            print("Please, insert a 5-letter word!")
+        elif not guess.isalpha():
+            print("Please, insert a 5-letter word!")
+        elif guess in history:
+            print("This guess is already tried, try another one!")
+        elif guess not in en_words_5:
+            print("Please, insert a word in the English dictionary")
+        else:
+            is_input_ok = True
+
+    if not is_stopped:
+        # check the correctness of letters
         c_letters, c_tiles = check_guess(guess, target)
 
+        # print a feedback
         print(c_letters)
         print(c_tiles)
 
+        # update history
+        history.append(guess)
         colored_letters.append(c_letters)
         colored_tiles.append(c_tiles)
 
-        # increase the counter
-        attempt += 1
+        if guess == target:
+            is_guessed = True
+        else:
+            # increase the counter
+            attempt += 1
+
 
 # provide a feedback when the target is missed
+if is_guessed:
+    print(f"Good job! the target is guessed in {attempt} times!")
+elif is_stopped:
+    print("Thank you for playing. Bye bye.")
+else:
+    print(f"You missed the target word: {target}. Play again.")
+
+
+# save the tries on a file
+if save_on_file:
+    # file modalities: w (write and overwrite) - a (append) - r (read)
+    f = open("wordle.txt", "a")
+    now = datetime.now()
+    now_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    f.write(f"Wordle is played on {now_string}.\n")
+    f.write(f"The target word is {target}.\n")
+    for i, g in enumerate(history):
+        f.write(f"Attempt {i+1}: {g}.\n")
+    f.write("\n")
+    f.close()
+
 
 sys.exit()
